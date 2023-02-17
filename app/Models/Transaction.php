a35ea7ca0fd0cc9997bc69b4c32c0cdb268c2798
jakaalakbar <jakaal17@gmail.com>
2023-02-18 00:12:56 +0700

    add model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'users_id',
+        'name',
+        'email',
+        'address',
+        'phone',
+        'courier',
+        'payment',
+        'payment_url',
+        'total_price',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
